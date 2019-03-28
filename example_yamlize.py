25ab9a57f944ddc3b022ceeb060e41263fbc2ca0
Matt J Williams <mattjw@mattjw.net>
2019-03-28 23:23:38 +0000

    Add another edge case -- nested dict full of defaults.

--- example_yamlize.py
+++ example_yamlize.py
@@ -5,13 +5,15 @@ from yamlize import Object, Attribute, Sequence
 
 DEFAULT_ELASTIC_HOST = "localhost"
 DEFAULT_ELASTIC_PORT = 9200
+DEFAULT_KAFKA_HOST = "kafka"
+DEFAULT_REDIS_HOST = "redis"
 
 #
 # Classes as schema
 
 class Node(Object):
-    host = Attribute(type=str)
-    port = Attribute(type=int)
+    host = Attribute(type=str, default=DEFAULT_ELASTIC_HOST)
+    port = Attribute(type=int, default=DEFAULT_ELASTIC_PORT)
 
 class Nodes(Sequence):
     item_type = Node
@@ -19,17 +21,23 @@ class Nodes(Sequence):
 class Elastic(Object):
     nodes = Attribute(type=Nodes)
 
+class RequiredDefaults(Object):
+    x = Attribute(type=str, default="val-x")
+    y = Attribute(type=str, default="val-y")
+
 class JobBatch(Object):
-    redis_host = Attribute(type=str)
+    redis_host = Attribute(type=str, default=DEFAULT_REDIS_HOST)
 
 class JobStreaming(Object):
-    kafka_broker = Attribute(type=str)
+    kafka_broker = Attribute(type=str, default=DEFAULT_KAFKA_HOST)
 
 class ConfigRoot(Object):
     my_int = Attribute(type=int)
     my_float = Attribute(type=float)
     my_str = Attribute(type=str)
     description = Attribute(type=str)
+
+    required_defaults = Attribute(type=RequiredDefaults, default=RequiredDefaults())
 
     job_batch = Attribute(type=JobBatch, default=None)
     job_streaming = Attribute(type=JobStreaming, default=None)
@@ -58,7 +66,8 @@ description: >
 
 out = ConfigRoot.load(raw)
 
-print("root", [f"<{f}>" for f in [out.my_int, out.my_float, out.my_str, out.description]])
-print("elastic", ".nodes", [(node.host, node.port)for node in out.elastic.nodes])
-print("job_batch", ".redis_host", out.job_batch.redis_host)
-print("job_streaming", out.job_streaming)
+print("root:               ", [f"<{f}>" for f in [out.my_int, out.my_float, out.my_str, out.description]])
+print("elastic:            ", ".nodes", [(node.host, node.port) for node in out.elastic.nodes])
+print("required_defaults:  ", ".x", out.required_defaults.x, "|", ".y", out.required_defaults.y)
+print("job_batch:          ", ".redis_host", out.job_batch.redis_host)
+print("job_streaming:      ", out.job_streaming)
